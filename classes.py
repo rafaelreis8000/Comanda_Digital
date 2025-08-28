cf40df9d848f97bc9f68bd94578e283b3cf49010
rafaelreis8000 <priv.rafa.reis@gmail.com>
2025-08-28 17:55:43 -0300

    Início de modelagem da classe Lanche

---
+++ classes.py
@@ -0,0 +1,7 @@
+class Lanche:
+    def __init__(self, nome, preço, ingredientes):
+        self.nome = nome
+        self.preço = preço
+        self.ingredientes = ingredientes
+
+burguer = Lanche('X-Burguer', 23.90, 'pão brioche, hambúrguer 150g, queijo cheddar e molho especial')
