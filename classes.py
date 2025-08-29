5e982b663ada975164cf632148a8b1e7efcbba00
Rafael Reis <contato.rafael.reis41@gmail.com>
2025-08-29 13:56:57 -0300

    Agregação das classes Lanche e Pedido

--- classes.py
+++ classes.py
@@ -1,7 +1,26 @@
 class Lanche:
-    def __init__(self, nome, preço, ingredientes):
+    def __init__(self, nome, ingredientes, valor):
         self.nome = nome
-        self.preço = preço
         self.ingredientes = ingredientes
+        self.valor = valor
 
-burguer = Lanche('X-Burguer', 23.90, 'pão brioche, hambúrguer 150g, queijo cheddar e molho especial')
+class Pedido:
+    def __init__(self):
+        self.itens = []
+
+    def adicionar_item(self, item):
+        self.itens.append(item)
+    
+    def mostrar_valor(self):
+        total = 0
+        for item in self.itens:
+            total += item.valor
+        return total
+    
+x_burguer = Lanche('X-Burguer', 'Pão brioche, hambúrguer artesanal 150g, queijo cheddar e molho especial', 29.90)
+
+pedido = Pedido()
+
+pedido.adicionar_item(x_burguer)
+pedido.adicionar_item(x_burguer)
+print(pedido.mostrar_valor())
