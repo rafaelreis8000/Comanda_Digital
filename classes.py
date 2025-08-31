f61fa8d18a9aabc36c618cf8102ba0575ae5e1e9
rafaelreis8000 <priv.rafa.reis@gmail.com>
2025-08-31 14:59:56 -0300

    mudança nas classes base

--- classes.py
+++ classes.py
@@ -1,18 +1,25 @@
 from sqlalchemy import create_engine, Integer, String, Float, Column
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-engine = create_engine('sqlite:///Proj_Lanches/database.db', echo = True)
+engine = create_engine('sqlite:///Proj_Lanches/Banco/database.db', echo = True)
+
+Session = sessionmaker(bind = engine)
+session = Session()
 
 Base = declarative_base()
 
-class Lanche(Base):
+class BaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
+
+class Lanche(BaseModel):
     __tablename__ = 'lanches'
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable = False)
     description = Column(String, nullable = False)
     value = Column(Float, nullable = False)
 
-class Pedido(Base):
+class Pedido(BaseModel):
     __tablename__ = 'pedidos'
     id = Column(Integer, primary_key = True)
     itens = Column()
