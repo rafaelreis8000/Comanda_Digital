cd9bc70060f0f62b87b9525703155b58a3a8eaf5
Rafael Reis <contato.rafael.reis41@gmail.com>
2025-09-01 16:28:51 -0300

    continuação da modelagem das tabelas do banco

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, Float, DateTime, ForeignKey
 from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
@@ -22,7 +22,7 @@ class Cliente(Base):
         self.sobrenome = sobrenome
         self.telefone = telefone
     
-    def cadastrar_cliente(self, nome, sobrenome, telefone):
+    def cadastrar_cliente(nome, sobrenome, telefone):
         cliente = Cliente(nome = nome, sobrenome = sobrenome, telefone = telefone)
         session.add(cliente)
         session.commit()
@@ -41,7 +41,7 @@ class Lanche(Base):
         self.descricao = descricao
         self.valor = valor
 
-    def criar_lanche(self, nome, descricao, valor):
+    def criar_lanche(nome, descricao, valor):
         lanche = Lanche(nome = nome, descricao = descricao, valor = valor)
         session.add(lanche)
         session.commit()
@@ -52,15 +52,32 @@ class Pedido(Base):
 
     id = Column('id', Integer, primary_key = True, nullable = False)
     cliente = Column('cliente', ForeignKey('clientes.id'))
-    data = Column('data',)##########################################
+    data = Column('data', DateTime)
 
     def __init__(self, cliente, data):
         self.cliente = cliente
         self.data = data
     
-    def fazer_pedido(self, cliente, data):##########################
-        pedido = Pedido(cliente, data)##############################
+    def fazer_pedido(cliente, data = datetime.today()):
+        pedido = Pedido(cliente = cliente, data = data)
         session.add(pedido)
         session.commit()
+
+
+class Pedido_Lanche(Base):
+    __tablename__ = 'pedidos_lanche'
+
+    id = Column('id', Integer, primary_key = True)
+    quantidade = Column('quantidade', Integer, nullable = False)
+
+    def __init__(self, id, quantidade):
+        self.id = id
+        self.quantidade = quantidade
+
+    def mostrar_pedido(id, quantidade):
+        pedido_lanche = Pedido_Lanche(id = "'pedido.id','lanche.id'", quantidade = 0)
+        session.add(pedido_lanche)
+        session.commit()
+
 
 Base.metadata.create_all(bind = db)
