d45ea5b7b3189afdbdb4ca15432ba5105e668395
Rafael Reis <contato.rafael.reis41@gmail.com>
2025-09-01 15:20:17 -0300

    Modelagem do banco de dados

---
+++ Test_projects/classes.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine, Integer, String, Float, Column
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+engine = create_engine('sqlite:///Proj_Lanches/Banco/database.db', echo = True)
+
+Session = sessionmaker(bind = engine)
+session = Session()
+
+Base = declarative_base()
+
+class BaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
+
+class Lanche(BaseModel):
+    __tablename__ = 'lanches'
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    descripion = Column(String, nullable = False)
+    value = Column(Float, nullable = False)
+
+class Pedido(BaseModel):
+    __tablename__ = 'pedidos'
+    id = Column(Integer, primary_key = True)
+    itens = Column()
--- classes.py
+++ classes.py
@@ -1,25 +1,66 @@
-from sqlalchemy import create_engine, Integer, String, Float, Column
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import sessionmaker, declarative_base
+from datetime import datetime
 
-engine = create_engine('sqlite:///Proj_Lanches/Banco/database.db', echo = True)
-
-Session = sessionmaker(bind = engine)
+db = create_engine('sqlite:///D:\GitHub\Proj_Lanches/banco.db')
+Session = sessionmaker(bind = db)
 session = Session()
 
 Base = declarative_base()
 
-class BaseModel(Base):
-    __abstract__ = True
-    __allow_unmapped__ = True
 
-class Lanche(BaseModel):
+class Cliente(Base):
+    __tablename__ = 'clientes'
+
+    id = Column('id', Integer, primary_key = True, autoincrement = True)
+    nome = Column('nome', String, nullable = False)
+    sobrenome = Column('sobrenome', String, nullable = False)
+    telefone = Column('telefone', Integer, nullable = False)
+
+    def __init__(self, nome, sobrenome, telefone):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.telefone = telefone
+    
+    def cadastrar_cliente(self, nome, sobrenome, telefone):
+        cliente = Cliente(nome = nome, sobrenome = sobrenome, telefone = telefone)
+        session.add(cliente)
+        session.commit()
+
+
+class Lanche(Base):
     __tablename__ = 'lanches'
-    id = Column(Integer, primary_key = True)
-    name = Column(String, nullable = False)
-    description = Column(String, nullable = False)
-    value = Column(Float, nullable = False)
 
-class Pedido(BaseModel):
+    id = Column('id', Integer, primary_key = True, autoincrement = True)
+    nome = Column('nome', String, nullable = False)
+    descricao = Column('descrição', String)
+    valor = Column('valor', Float, nullable = False)
+
+    def __init__(self, nome, descricao, valor):
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+
+    def criar_lanche(self, nome, descricao, valor):
+        lanche = Lanche(nome = nome, descricao = descricao, valor = valor)
+        session.add(lanche)
+        session.commit()
+
+
+class Pedido(Base):
     __tablename__ = 'pedidos'
-    id = Column(Integer, primary_key = True)
-    itens = Column()
+
+    id = Column('id', Integer, primary_key = True, nullable = False)
+    cliente = Column('cliente', ForeignKey('clientes.id'))
+    data = Column('data',)##########################################
+
+    def __init__(self, cliente, data):
+        self.cliente = cliente
+        self.data = data
+    
+    def fazer_pedido(self, cliente, data):##########################
+        pedido = Pedido(cliente, data)##############################
+        session.add(pedido)
+        session.commit()
+
+Base.metadata.create_all(bind = db)
